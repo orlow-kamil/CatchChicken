80d5e5cb5abd9603ca978a0a363e0ad5a46e2f3e
Kamil Orłow <40249705+orlow-kamil@users.noreply.github.com>
2020-08-01 12:33:56 +0200

    Add scripts, which contain chicken stats
    - Scripts: ChickenData
    - Tests: ChickenDataTests
    - Example: NormalChickenData as ScriptableObject

---
+++ CatchChicken/Assets/Scripts/ChickenData.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "ChickenData", menuName = "ScriptableObjects/ChickenData")]
+public class ChickenData : ScriptableObject
+{
+    public static readonly float MaxStunTime = 3.0f;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float stunTime;
+    [SerializeField][Range(0,1)] private float catchable;
+
+    public float Speed 
+    {
+        get => speed; 
+        set
+        {
+            speed = (value >= 0)
+                ? value
+                : 0f;
+        }
+    }
+    public float StunTime 
+    {
+        get => stunTime; 
+        set
+        {
+            stunTime = (value < 0)
+                ? 0f
+                : (value > MaxStunTime)
+                    ? MaxStunTime
+                    : value;
+        }
+    }
+    public float Catchable 
+    {
+        get => catchable; 
+        set
+        {
+            catchable = (value < 0)
+                ? 0f
+                : (value > 1)
+                    ? 1f
+                    : value;
+        }
+    }
+}
+
---
+++ CatchChicken/Assets/Tests/EditMode/ChickenDataTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public partial class ChickenDataTests
+    {
+        public partial class ChickenProperty
+        {
+            ChickenData chickenData;
+
+            [SetUp]
+            public void InitClass()
+            {
+                chickenData = Substitute.For<ChickenData>();
+            }
+            // A Test behaves as an ordinary method
+            [Test]
+            public void Should_ReturnObject_When_InitialClass()
+            {
+                Assert.IsNotNull(chickenData);
+            }
+
+            [Test]
+            public void Should_ReturnSpeedValue_When_SpeedValueIsSet()
+            {
+                chickenData.Speed = 1f;
+                Assert.AreEqual(1f, chickenData.Speed);
+            }
+
+            [Test]
+            public void Should_ReturnZero_When_SpeedValueIsSetNegative()
+            {
+                chickenData.Speed = -1f;
+                Assert.AreEqual(0f, chickenData.Speed);
+            }
+
+            [Test]
+            public void Should_ReturnStunTime_When_StunTimeIsSet()
+            {
+                chickenData.StunTime = 1f;
+                Assert.AreEqual(1f, chickenData.StunTime);
+            }
+
+            [Test]
+            public void Should_ReturnZero_When_StunTimeIsSetNegative()
+            {
+                chickenData.StunTime = -1f;
+                Assert.AreEqual(0f, chickenData.StunTime);
+            }
+
+            [Test]
+            public void Should_ReturnMaxValue_When_StunTimeIsSetOverMaxStunTime()
+            {
+                chickenData.StunTime = 3f;
+                Assert.AreEqual(ChickenData.MaxStunTime, chickenData.StunTime);
+            }
+
+            [Test]
+            public void Should_ReturnCatchable_When_CatchableIsSet()
+            {
+                chickenData.Catchable = .5f;
+                Assert.AreEqual(.5f, chickenData.Catchable);
+            }
+
+            [Test]
+            public void Should_ReturnZero_When_CatchableIsSetNegative()
+            {
+                chickenData.Catchable = -1f;
+                Assert.AreEqual(0f, chickenData.Catchable);
+            }
+
+            [Test]
+            public void Should_ReturnOne_When_CatchableIsSetOverOne()
+            {
+                chickenData.Catchable = 1.5f;
+                Assert.AreEqual(1f, chickenData.Catchable);
+            }
+        }
+    }
+}
